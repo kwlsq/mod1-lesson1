204ae5006ce1e5e70d9f7ca4d8bdbf251fa90b03
Vincentius <vincentiussss@gmail.com>
2025-02-03 15:10:26 +0700

    improve exercise 4 UI

--- src/main/java/org/vincent/Main.java
+++ src/main/java/org/vincent/Main.java
@@ -58,7 +58,14 @@ public class Main {
     public static void calculateDateDifference(String dateBefore, String dateAfter){
         LocalDate dateBeforeConverted = LocalDate.parse(dateBefore, DateTimeFormatter.ISO_DATE);
         LocalDate dateAfterConverted = LocalDate.parse(dateAfter, DateTimeFormatter.ISO_DATE);
-        System.out.println(ChronoUnit.DAYS.between(dateBeforeConverted,dateAfterConverted));
+        long dayDiff = (ChronoUnit.DAYS.between(dateBeforeConverted,dateAfterConverted));
+
+        System.out.println("Exercise 4: " +
+                "\n" + "date1 = " + dateBefore +
+                "\n" + "date2 = " + dateAfter +
+                "\n" + "day difference = " + dayDiff +
+                "\n"
+        );
     }
 
     //Exercise 5
