9fd8f92978ac4b1f1f586a0e0177e6794eec3ab2
Vincentius <vincentiussss@gmail.com>
2025-02-03 14:50:35 +0700

    module 1 lesson 1

--- src/main/java/org/vincent/Main.java
+++ src/main/java/org/vincent/Main.java
@@ -1,23 +1,67 @@
 package org.vincent;
 
-import java.util.ArrayList;
-import java.util.List;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+
+import static java.lang.Math.pow;
+
 public class Main {
-    static float PHI = 3.14f;
-    public static void main(String[] args) {
-        String[] intArray = {"hi","hello"};
-        List<String> stringList = new ArrayList<>();
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.print("Hello and welcome!");
+    static double PHI = 3.14159;
+    //Exercise 1
+    public static void calculateRectangleArea(int rectangleLength, int rectangleWidth){
+        System.out.println(rectangleLength*rectangleWidth);
+    }
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+    //Exercise 2
+    public static void calculateCircleDiameter(int circleRadius){
+        System.out.println(circleRadius*2);
+    }
+
+    public static void calculateCircleCircumference(int circleRadius){
+        System.out.println(2*PHI*circleRadius);
+    }
+
+    public static void calculateCircleArea(int circleRadius){
+        System.out.println(PHI*(pow(circleRadius,2)));
+    }
+
+    //Exercise 3
+    public static void calculateTriangleAngle(int firstAngle, int secondAngle){
+        System.out.println(180-(firstAngle+secondAngle));
+    }
+
+    //Exercise 4
+    public static void calculateDateDifference(String dateBefore, String dateAfter){
+        LocalDate dateBeforeConverted = LocalDate.parse(dateBefore, DateTimeFormatter.ISO_DATE);
+        LocalDate dateAfterConverted = LocalDate.parse(dateAfter, DateTimeFormatter.ISO_DATE);
+        System.out.println(ChronoUnit.DAYS.between(dateBeforeConverted,dateAfterConverted));
+    }
+
+    //Exercise 5
+    public static void extractNameInitials(String fullName){
+        String[] name = fullName.split(" ");
+        StringBuilder initials = new StringBuilder();
+
+        for(String brokeDownName : name){
+            initials.append(brokeDownName.charAt(0));
         }
+
+        System.out.println(initials);
+    }
+
+    public static void main(String[] args) {
+        calculateRectangleArea(5,3);
+
+        calculateCircleDiameter(5);
+        calculateCircleCircumference(5);
+        calculateCircleArea(5);
+
+        calculateTriangleAngle(80,65);
+
+        calculateDateDifference("2024-03-19","2024-03-21");
+
+        extractNameInitials("John Doe");
     }
 }
